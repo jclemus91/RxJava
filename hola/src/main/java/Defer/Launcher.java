35a6e764a62df1a0a1b0b95de3db99f0da7aa87f
Julio Lemus <julio.lemus.lara@gmail.com>
2019-05-29 22:44:49 -0500

    callable, connectableObservable, DEfer, empty, never

---
+++ hola/src/main/java/Defer/Launcher.java
@@ -0,0 +1,22 @@
+package Defer;
+
+import io.reactivex.Observable;
+
+public class Launcher {
+    private static int start = 1;
+    private static int count = 5;
+
+    public static void main(String[] args) {
+        //NO DEFER
+        //Observable<Integer> source = Observable.range(start,count);
+
+        //DEFER
+        Observable<Integer> source = Observable.defer(() -> Observable.range(start, count));
+
+        source.subscribe(i -> System.out.println("Observer 1: " + i));
+        //modify count
+        count = 10;
+        source.subscribe(i -> System.out.println("Observer 2: " + i));
+    }
+}
+
