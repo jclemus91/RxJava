a7f425bf3082359262fdc76a45eb83f7fb6487f3
Julio Lemus <julio.lemus.lara@gmail.com>
2019-05-30 22:54:44 -0500

    Observables and subscribers chapter finished

---
+++ ObserversSubscribers/hola/src/main/java/Disposable2/Launcher.java
@@ -0,0 +1,29 @@
+package Disposable2;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.ResourceObserver;
+import java.util.concurrent.TimeUnit;
+public class Launcher {
+    public static void main(String[] args) {
+        Observable<Long> source =
+                Observable.interval(1, TimeUnit.SECONDS);
+        ResourceObserver<Long> myObserver = new
+                ResourceObserver<Long>() {
+                    @Override
+                    public void onNext(Long value) {
+                        System.out.println(value);
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+                    @Override
+                    public void onComplete() {
+                        System.out.println("Done!");
+                    }
+                };
+        //capture Disposable
+        Disposable disposable = source.subscribeWith(myObserver);
+    }
+}
